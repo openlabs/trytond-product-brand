840755071f9e91c1c61bd979fe98aa8c3383f797
uditagarwal <udit.agarwal@openlabs.co.in>
2013-12-10 17:34:47 +0530

    Added boilerplate

---
+++ __init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__
+
+    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
+    :license: BSD, see LICENSE for more details.
+"""
+from trytond.pool import Pool
+from brand import Brand
+
+
+def register():
+    Pool.register(
+        Brand,
+        module='brands', type_='model'
+    )
---
+++ setup.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+
+from setuptools import setup
+import re
+import os
+import ConfigParser
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+config = ConfigParser.ConfigParser()
+config.readfp(open('tryton.cfg'))
+info = dict(config.items('tryton'))
+for key in ('depends', 'extras_depend', 'xml'):
+    if key in info:
+        info[key] = info[key].strip().splitlines()
+major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
+major_version = int(major_version)
+minor_version = int(minor_version)
+
+requires = []
+
+MODULE = 'brands'
+PREFIX = 'trytond'
+
+for dep in info.get('depends', []):
+    if not re.match(r'(ir|res|webdav)(\W|$)', dep):
+        requires.append('trytond_%s >= %s.%s, < %s.%s' % (
+            dep, major_version, minor_version,
+            major_version, minor_version + 1)
+        )
+requires.append(
+    'trytond >= %s.%s, < %s.%s' % (
+        major_version, minor_version, major_version, minor_version + 1
+    )
+)
+setup(
+    name='%s_%s' % (PREFIX, MODULE),
+    version=info.get('version', '0.0.1'),
+    description="Brands Module for Tryton",
+    author="Openlabs Technologies & consulting (P) Limited",
+    author_email='info@openlabs.co.in',
+    url='http://www.openlabs.co.in',
+    package_dir={'trytond.modules.%s' % MODULE: '.'},
+    packages=[
+        'trytond.modules.%s' % MODULE,
+        'trytond.modules.%s.tests' % MODULE,
+    ],
+    package_data={
+        'trytond.modules.%s' % MODULE: info.get('xml', [])
+        + info.get('translation', [])
+        + ['tryton.cfg', 'locale/*.po', 'tests/*.rst', '*.odt']
+        + ['view/*.xml'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Tryton',
+        'Topic :: Office/Business',
+    ],
+    license='GPL-3',
+    install_requires=requires,
+    zip_safe=False,
+    entry_points="""
+    [trytond.modules]
+    %s = trytond.modules.%s
+    """ % (MODULE, MODULE),
+    test_suite='tests',
+    test_loader='trytond.test_loader:Loader',
+)
